5bf8fe1bcf74b6155fd0d7c82f55c03a24ef3994
vislee <louivislee@gmail.com>
2018-11-08 21:11:26 +0800

    push version 0.02

--- lib/resty/setcookie.lua
+++ lib/resty/setcookie.lua
@@ -21,12 +21,13 @@ end
 local _M = tab_new(0, 6)
 _M.version = "0.02"
 
+local _get_cookie_tab
 local mt = { __index = _M }
 function  _M.new()
-    local cookie = ngx.header["set-cookie"] or tab_new(3, 0)
+    local tmp_cookie = ngx.header["set-cookie"] or ""
+
     local t = {
-        _cookie = cookie,
-        _cookie_cache = nil,
+        _cookie = _get_cookie_tab(tmp_cookie),
     }
 
     return setmetatable(t, mt)
@@ -53,7 +54,7 @@ local function _parse_cookie(text_cookie)
             m = m + 1
         end
     end
-    local tab = tab_new(n - m + 1, m + 1)
+    local tab = tab_new(n - m + 1, m + 2)
 
     local EXPECT_KEY    = 1
     local EXPECT_VALUE  = 2
@@ -79,9 +80,10 @@ local function _parse_cookie(text_cookie)
                     val = str_sub(text_cookie, j, i)
                 else
                     val = str_sub(text_cookie, j, i-1)
+                    i = i + 1
                 end
                 state = EXPECT_SP
-                j = i + 1
+                j = i
             end
 
         elseif state == EXPECT_VALUE then
@@ -90,13 +92,15 @@ local function _parse_cookie(text_cookie)
                     val = str_sub(text_cookie, j, i)
                 else
                     val = str_sub(text_cookie, j, i-1)
+                    i = i + 1
                 end
                 state = EXPECT_SP
-                j = i + 1
+                j = i
             end
+        end
 
-        else -- state == EXPECT_SP
-            if not key and val then
+        if state == EXPECT_SP then
+            if key == nil and val then
                 tab[#tab+1] = val
             elseif key and val then
                 tab[key] = val
@@ -112,31 +116,35 @@ local function _parse_cookie(text_cookie)
         i = i + 1
     end
 
+    tab["_string"] = text_cookie
+
     return cookie_key, tab
 end
 
 
-local function _get_cookie_tab(cookie)
+_get_cookie_tab = function(cookie)
     local tab
 
-    if type(cookie) == "table" then
+    if cookie ~= nil and type(cookie) == "table" then
         tab = tab_new(0, #cookie + 3)
         for i, ck in ipairs(cookie) do
             local k, t = _parse_cookie(ck)
             if k == nil then
                 ngx.log(ngx.ERR, "err cookie:", ck)
             else
-                t._index = i
                 tab[k] = t
             end
         end
     else
         tab = tab_new(0, 3)
+        if cookie == nil or cookie == "" then
+            return tab
+        end
+
         local k, t = _parse_cookie(cookie)
         if k == nil then
             ngx.log(ngx.ERR, "err cookie:", cookie)
-        else
-            t._index = 1
+        elseif #k > 0 then
             tab[k] = t
         end
     end
@@ -146,92 +154,49 @@ end
 
 
 function _M.get(self, key)
-    if not self._cookie_cache then
-        self._cookie_cache = _get_cookie_tab(self._cookie) or tab_new(1, 1)
-    end
+    local ck = self._cookie[key]
 
-    local ck = self._cookie_cache[key]
-    if not ck then
-        return {}
-    end
-
-    local copy_ck = tab_new(3, #ck)
-    for k, v in pairs(ck) do
-        if not k then
-            copy_ck[#copy_ck+1] = v
-        elseif k ~= "_index" then
-            copy_ck[k] = v
-        end
-    end
-
-    return copy_ck
+    return ck
 end
 
 
 function _M.del(self, key)
-    if not self._cookie_cache then
-        self._cookie_cache = _get_cookie_tab(self._cookie) or tab_new(1, 1)
-    end
-
-    local ck = self._cookie_cache[key]
-    if not ck then
-        return
-    end
-
-    self._cookie[ck._index] = ""
-    self._cookie_cache[key] = nil
-
-    ck._index = nil
+    local ck = self._cookie[key]
+    self._cookie[key] = nil
 
     return ck
 end
 
 
 function _M.set(self, text_cookie)
-    if not self._cookie_cache then
-        self._cookie_cache = _get_cookie_tab(self._cookie) or tab_new(1, 1)
-    end
-
     if type(text_cookie) ~= "string" then
         return nil, "text_cookie is not string"
     end
 
     local key, cookie = _parse_cookie(text_cookie)
-
     if not key then
         return nil, "not have key"
     end
 
-    local old_cookie = self._cookie_cache[key]
-    if old_cookie and old_cookie._index then
-        self._cookie[old_cookie._index] = ""
-        old_cookie._index = nil
-    end
+    local old_cookie = self._cookie[key] or {}
+    self._cookie[key] = cookie
 
-    self._cookie[#self._cookie+1] = text_cookie
-    cookie._index = #self._cookie
-    self._cookie_cache[key] = cookie
-
-    return old_cookie or {}
+    return old_cookie
 end
 
 
 function _M.set_cookie(self)
-    local c = self._cookie_cache
-    self._cookie_cache = nil
+    local tmp = tab_new(#self._cookie + 1, 0)
 
-    ngx.log(ngx.INFO, "====", cjson.encode(self._cookie))
-
-    local tmp = {}
-    for _, ck in ipairs(self._cookie) do
-        if ck ~= nil and type(ck) == "string" and ck ~= "" then
-            tmp[#tmp+1] = ck
+    for _, ck in pairs(self._cookie) do
+        if ck ~= nil and type(ck) == "table" and ck["_string"] ~= nil then
+            tmp[#tmp+1] = ck["_string"]
         end
     end
+    ngx.log(ngx.INFO, "====", cjson.encode(tmp))
+
     ngx.header["set-cookie"] = tmp
-    -- if c and type(c) == "table" then
-    --     tab_clear(c)
-    -- end
+    tab_clear(tmp)
 end
 
 
--- t/setcookie.t
+++ t/setcookie.t
@@ -87,7 +87,101 @@ set-cookie: test=testcookie; path=/
 --- request
 GET /t
 --- response_headers_like
-set-cookie: test=testcookie; path=/
+set-cookie: test=testcookie; path=/, foo=bar; path=/
+--- error_code: 200
+--- no_error_log
+[error]
+
+
+=== TEST 3: cookie:del
+--- http_config
+    lua_package_path 'lib/?.lua;;';
+
+    init_by_lua_block {
+        require 'luacov.tick'
+        jit.off()
+    }
+
+    server {
+        listen 127.0.0.1:9082;
+
+        location / {
+            content_by_lua_block {
+                ngx.header["set-cookie"] = {"test=test; path=/", "foo=bar; path=/"}
+                ngx.say("ok")
+            }
+        }
+    }
+
+--- config
+    location /t {
+        proxy_pass http://127.0.0.1:9082/;
+
+        header_filter_by_lua_block {
+            local cjson = require "cjson.safe"
+            local setck = require "resty.setcookie"
+            local cookie = setck:new()
+            local ck, err = cookie:del("test")
+            if err ~= nil then
+                ngx.log(ngx.ERR, err)
+            end
+            ngx.log(ngx.INFO, "del cookie:", cjson.encode(ck))
+            cookie:set_cookie()
+        }
+    }
+
+--- request
+GET /t
+--- response_headers_like
+set-cookie: foo=bar; path=/
+--- error_code: 200
+--- no_error_log
+[error]
+
+
+
+=== TEST 4: cookie:get
+--- http_config
+    lua_package_path 'lib/?.lua;;';
+
+    init_by_lua_block {
+        require 'luacov.tick'
+        jit.off()
+    }
+
+    server {
+        listen 127.0.0.1:9082;
+
+        location / {
+            content_by_lua_block {
+                ngx.header["set-cookie"] = {"test=test; path=/; http", "foo=bar; path=/"}
+                ngx.say("ok")
+            }
+        }
+    }
+
+--- config
+    location /t {
+        proxy_pass http://127.0.0.1:9082/;
+
+        header_filter_by_lua_block {
+            local cjson = require "cjson.safe"
+            local setck = require "resty.setcookie"
+            local cookie = setck:new()
+            local ck = cookie:get("test")
+            for k, v in ipairs(ck) do
+                ngx.log(ngx.INFO, k, "====", v)
+            end
+            if ck == nil or type(ck) ~= "table" or ck["test"] ~= "test" or ck["path"] ~= "/" then
+                ngx.log(ngx.ERR, "cookie get error")
+            end
+        }
+    }
+
+--- request
+GET /t
+--- response_headers_like
+set-cookie: test=test; path=/; http, foo=bar; path=/
 --- error_code: 200
 --- no_error_log
 [error]
